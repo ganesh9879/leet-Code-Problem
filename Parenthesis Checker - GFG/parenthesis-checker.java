fc04f4364f05d751d3e55b2559e4ee0c251050d1
ganesh9879 <94919253+ganesh9879@users.noreply.github.com>
2022-05-25 22:56:38 +0530

    Added solution - LeetHub

---
+++ Parenthesis Checker - GFG/parenthesis-checker.java
@@ -0,0 +1,73 @@
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Driverclass
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        //Reading total number of testcases
+        int t= sc.nextInt();
+        
+        while(t-- >0)
+        {
+            //reading the string
+            String st = sc.next();
+            
+            //calling ispar method of Paranthesis class 
+            //and printing "balanced" if it returns true
+            //else printing "not balanced"
+            if(new Solution().ispar(st) == true)
+                System.out.println("balanced");
+            else
+                System.out.println("not balanced");
+        
+        }
+    }
+}// } Driver Code Ends
+
+
+
+
+class Solution
+{
+    //Function to check if brackets are balanced or not.
+    static boolean ispar(String x)
+    {
+    
+    //Function to check if brackets are balanced or not.
+        Stack<Character> stack = new Stack<>();
+       for(int i=0; i<x.length(); i++){
+           char c = x.charAt(i);
+       
+       
+       if(c=='(' || c=='{' || c=='['){
+           stack.push(c);
+           continue;
+       }
+       else if (stack.isEmpty()){
+           return false;
+       }
+       
+       if(c==')' &&  stack.peek()!='(')
+       break;
+       
+       else if(c=='}' && stack.peek()!='{')
+       break;
+       
+       else if(c==']' && stack.peek()!='[')
+       break;
+       
+       else{
+           stack.pop();
+           
+        }
+        
+   }
+      return stack.isEmpty(); 
+   
+}
+}
